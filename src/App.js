e58bb39870a5c7f08a9489c9dc8ccefffdeda002
Marisa Vitale <marisa.vitale2306@gmail.com>
2023-03-12 16:02:55 +0100

    not found route

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App () {
         <Route path={ROUTES.DETAIL} element={<PodcastDetails />}>
           <Route path={ROUTES.EPISODE} element={<EpisodeDetails />} />
         </Route>
+        <Route path='*' element={<h1>404 Page not found</h1>}></Route>
       </Routes>
     </div>
   )
